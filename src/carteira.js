1ed5733c904939348712d3cabc4c45ba146fdfe8
Bruno Figueiredo <brunonf15@gmail.com>
2025-07-25 12:06:46 +0100

    Primeiro commit

---
+++ src/carteira.js
@@ -0,0 +1,29 @@
+class Carteira {
+
+    constructor() {
+        this._saldo = 0;
+    }
+
+    consutarSaldo() {
+        return this._saldo;
+    }
+
+    depositar(valor) {
+        if (valor <= 0 || typeof valor !== 'number') {
+            throw new Error('O valor do depósito tem que ser positivo');
+        }
+        this._saldo += valor;
+    }
+
+    gastar(valor) {
+        if (valor <= 0 || typeof valor !== 'number') {
+            throw new Error('O valor do depósito tem que ser positivo');
+        }
+        if (valor > this._saldo) {
+            throw new Error('Saldo insuficiente');
+        }
+        this._saldo -= valor;
+    }
+
+}
+module.exports = Carteira;
